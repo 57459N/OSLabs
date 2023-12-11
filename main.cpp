81a7c62d2a82b5ef7d4ca04d64be4d786421f5d9
Stas Indyukov <stasun008@gmail.com>
2023-12-11 17:29:08 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
 
 #define CHUNK_NAME_CAP 64
-#define CHUNK_DATA_CAP 128
+#define CHUNK_DATA_CAP 32
 #define CHUNKS_CAP 128
 
-#define LIST_END (-1)
+#define ERR (CHUNKS_CAP + 1)
+#define LIST_END (CHUNKS_CAP + 1)
 
 typedef struct Chunk {
     size_t prepad = 0xAAAAAAAAAAAAAAAA;
@@ -16,48 +19,168 @@ typedef struct Chunk {
 } Chunk;
 
 typedef struct {
-    Chunk chinks[CHUNKS_CAP];
+    Chunk chunks[CHUNKS_CAP];
 } Filesystem;
 
 Filesystem FS{};
 
-size_t find_empty_block(){
+size_t find_empty_chunk() {
     for (int i = 0; i < CHUNKS_CAP; ++i) {
-        if (FS.chinks[i].name[0] == '\0'){
+        if (FS.chunks[i].name[0] == '\0') {
             return i;
         }
     }
-
-    return -1;
+    fprintf(stderr, "No empty chunks\n");
+    return ERR;
 }
 
-void create_file(const char* file_path, char* src, size_t size){
-
+size_t find_first_chunk(const char *file_path) {
+    for (int i = 0; i < CHUNKS_CAP; ++i) {
+        if (strcmp(FS.chunks[i].name, file_path) == 0) {
+            return i;
+        }
+    }
+    fprintf(stderr, "File {%s} not found\n", file_path);
+    return ERR;
 }
 
-void remove_file(const char* file_path){
+size_t create_file(const char *file_path, char *src, size_t size) {
 
+    if (size == 0)
+        return LIST_END;
+
+    size_t chunk_id = find_empty_chunk();
+
+    if (chunk_id == ERR) {
+        return LIST_END;
+    }
+
+    size_t to_write = size > CHUNK_DATA_CAP ? CHUNK_DATA_CAP : size;
+
+    memcpy(FS.chunks[chunk_id].data, src, to_write);
+
+    memcpy(FS.chunks[chunk_id].name, file_path, CHUNK_NAME_CAP);
+    FS.chunks[chunk_id].name[CHUNK_NAME_CAP - 1] = '\0';
+
+    FS.chunks[chunk_id].size = to_write;
+    src += to_write;
+    size -= to_write;
+
+    FS.chunks[chunk_id].next = create_file(file_path, src, size);
+
+    return chunk_id;
 }
 
-void copy_file(const char* file_path_dst, const char* file_path_src){
+void view_file(const char *file_path) {
+    size_t chunk_id = find_first_chunk(file_path);
 
+    if (chunk_id == ERR)
+        return;
+
+    while (chunk_id != LIST_END) {
+        size_t s = FS.chunks[chunk_id].size;
+        fwrite(FS.chunks[chunk_id].data, sizeof(FS.chunks[chunk_id].data[0]), s, stdout);
+        chunk_id = FS.chunks[chunk_id].next;
+        fflush(stdout);
+    }
 }
 
-void move_file(const char file_path_dst, const char* file_path_src){
+void remove_file(const char *file_path) {
 
+    size_t chunk_id = find_first_chunk(file_path);
+
+    if (chunk_id == ERR)
+        return;
+
+    while (chunk_id != LIST_END) {
+        FS.chunks[chunk_id].name[0] = '\0';
+        chunk_id = FS.chunks[chunk_id].next;
+    }
 }
 
-void view_file(const char* file_path){
+void copy_file(const char *file_path_dst, const char *file_path_src) {
+    size_t ext_chunk_id = find_first_chunk(file_path_src);
+    size_t new_chunk_id = find_empty_chunk();
 
+    if (ext_chunk_id == ERR)
+        return;
+
+    while (true) {
+        if (new_chunk_id == ERR) {
+            break;
+        }
+
+
+        memcpy(&FS.chunks[new_chunk_id], &FS.chunks[ext_chunk_id], sizeof(Chunk));
+
+        memcpy(FS.chunks[new_chunk_id].name, file_path_dst, CHUNK_NAME_CAP);
+        FS.chunks[new_chunk_id].name[CHUNK_NAME_CAP - 1] = '\0';
+
+        size_t new_next = find_empty_chunk();
+        if (FS.chunks[ext_chunk_id].next == LIST_END || new_next == ERR) {
+            new_next = LIST_END;
+            break;
+        } else {
+            FS.chunks[new_chunk_id].next = new_next;
+            new_chunk_id = new_next;
+            ext_chunk_id = FS.chunks[ext_chunk_id].next;
+        }
+    }
 }
 
-void print_files(){
+void move_file(const char *file_path_dst, const char *file_path_src) {
+    size_t chunk_id = find_first_chunk(file_path_src);
 
+    if (chunk_id == ERR)
+        return;
+
+    while (chunk_id != LIST_END) {
+        memcpy(FS.chunks[chunk_id].name, file_path_dst, CHUNK_NAME_CAP);
+        FS.chunks[chunk_id].name[CHUNK_NAME_CAP - 1] = '\0';
+        chunk_id = FS.chunks[chunk_id].next;
+    }
 }
 
+void print_files() {
+    for (int i = 0; i < CHUNKS_CAP; ++i) {
+        if (FS.chunks[i].next == LIST_END && FS.chunks[i].name[0] != '\0') {
+            printf("%s\n", FS.chunks[i].name);
+        }
+    }
+}
+
+void dump_fs(const char *bin_path) {
+    FILE *out = fopen(bin_path, "wb");
+    if (!out) {
+        fprintf(stderr, "Cannot dump filesystem\n");
+        return;
+    }
+
+    fwrite(FS.chunks, sizeof(Chunk), CHUNKS_CAP, out);
+    fclose(out);
+}
 
 
 int main() {
-    system("ls -l");
+    create_file("hello.txt", "asd", 3);
+    create_file("big.txt", "1234567890qwertyuiopasdfghjklpp\nxc", 34);
+    view_file("big.txt");
+    copy_file("big_copy.txt", "big.txt");
+    dump_fs("test.fs");
+
+    remove_file("big.txt");
+    printf("\n");
+    view_file("big_copy.txt");
+    dump_fs("test.fs");
+
+    printf("\n");
+    print_files();
+    move_file("big_copy_moved.txt", "big_copy.txt");
+    printf("\n");
+    print_files();
+    view_file("big_copy_moved.txt");
+    dump_fs("test.fs");
+
+
     return 0;
 }
